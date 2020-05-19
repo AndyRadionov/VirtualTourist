e87d4e25c0c2e69fe17771171b508eaea30a443a
Andrey Radionov <andyomsk@gmail.com>
2020-05-19 22:15:10 +0600

    Controllers designed

---
+++ VirtualTourist/controller/PhotoAlbumViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Andy on 19.05.2020.
+//  Copyright © 2020 AndyRadionov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
