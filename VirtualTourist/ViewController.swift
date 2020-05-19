c1c9eea4ca7825b8441e7afaf288cb27bb722193
Andrey Radionov <andyomsk@gmail.com>
2020-05-19 09:10:19 +0600

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Andy on 19.05.2020.
+//  Copyright © 2020 AndyRadionov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
